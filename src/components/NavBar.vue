4ecf6526d8d1ef0b16c2879ba2c1a5401bfbdfc2
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2022-08-04 20:58:56 -0600

    Added Vuesax ts is acting up and doesn't like the module since it's untyped, wil fix tomorrow

---
+++ src/components/NavBar.vue
@@ -0,0 +1,27 @@
+// This library hates TS and Vuesax pls fix
+<template>
+  <div>
+    <vs-navbar shadow-scroll fixed center-collapsed v-model="active">
+      <template #left>
+        <h3>Hello World</h3>
+      </template>
+      <vs-navbar-item :active="active == 'home'">
+        Home
+      </vs-navbar-item>
+      <template #right>
+        <vs-button flat>Posts</vs-button>
+      </template>
+    </vs-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data: () => {
+    return {
+      active: 'home'
+    }
+  }
+}
+</script>
